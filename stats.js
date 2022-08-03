f236f45da90a08e929860049886de7cfbb6d274f
Luke <luke@silicon.digital>
2022-08-03 14:17:56 +0100

    Added tweets to Milestones tab.

--- stats.js
+++ stats.js
@@ -123,17 +123,17 @@ function buildTabs() {
 
   getTabs().forEach((tab) => {
     var gameNavigationTabs = document.getElementById("horizontal-tabs");
-    var newTab = gameNavigationTabs.lastChild.cloneNode(true);
+    var newTab = gameNavigationTabs.lastElementChild.cloneNode(true);
     var tabTitle = newTab.getElementsByClassName('text-lead')[0];
 
     tabTitle.textContent = tab.title;
     newTab.classList.remove("tab-game-instances");
     newTab.classList.add(`tab-${tab.id}`);
     newTab.id = `tab-${tab.id}`;
-    newTab.firstChild.href = tab.href ? tab.href : `#${tab.id}`;
+    newTab.firstElementChild.href = tab.href ? tab.href : `#${tab.id}`;
 
     if (tab.target) {
-      newTab.firstChild.target = tab.target;
+      newTab.firstElementChild.target = tab.target;
     }
 
     gameNavigationTabs.appendChild(newTab);
@@ -284,7 +284,7 @@ function buildMilestonesTab() {
   const milestonesTable = document.createElement('table');
   milestonesTable.classList.add('table');
   milestonesTable.classList.add('table-striped');
-  milestonesTable.innerHTML = '<thead><tr><th class="text-label">Milestone</th><th class="text-label">Achived</th></tr></thead><tbody id="milestones-table"></tbody>';
+  milestonesTable.innerHTML = '<thead><tr><th class="text-label">Milestone</th><th class="text-label">Achived</th><th class="text-label">Tweets</th></tr></thead><tbody id="milestones-table"></tbody>';
 
   if (!Object.keys(gameData.milestones).length) {
     const messageBanner = document.createElement('div');
@@ -296,12 +296,18 @@ function buildMilestonesTab() {
 
     return;
   }
+
+  console.log(gameData.milestones);
+
   milestonesContainer[0].appendChild(milestonesTable);
 
   Object.keys(gameData.milestones).reverse().forEach((milestoneIndex) => {
     const milestone = gameData.milestones[milestoneIndex];
     const milestoneEntry = document.createElement('tr');
-    milestoneEntry.innerHTML = `<td>${milestone.value} ${milestone.type}</td><td>${milestone.achieved}</td>`;
+
+    const svg = `<a href="${milestone.tweet}"><svg class="romonitor-milestone-social-item" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#1DA1F2" d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg></a></div>`
+
+    milestoneEntry.innerHTML = `<td>${milestone.value} ${milestone.type}</td><td>${milestone.achieved}</td><td class="romonitor-tableitem">${svg}</td>`;
 
     document.getElementById('milestones-table').appendChild(milestoneEntry);
   });
